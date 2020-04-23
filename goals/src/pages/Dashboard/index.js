a6acdc507200e129b0344f17719e819af1f43611
Lucas Mendonça <lucasmendonca.dev@outlook.com>
2020-04-23 18:57:38 -0300

    feat: button floating and title

--- goals/src/pages/Dashboard/index.js
+++ goals/src/pages/Dashboard/index.js
@@ -1,15 +1,67 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TouchableOpacity } from 'react-native';
+import ActionButton from 'react-native-action-button';
 
 // import { Container } from './styles';
 
 export default function Dashboard({ navigation }) {
     return (
-        <View>
-            <Text>Dashboard</Text>
-            <Button
-                title="clique para a pagina de Details"
-                onPress={() => navigation.navigate('Details')}
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: 20,
+                backgroundColor: '#fff',
+            }}
+        >
+            <Text style={{ fontSize: 22, marginBottom: 15 }}>
+                Goals and Wishes
+            </Text>
+
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                }}
+            >
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: '#d3d3d3',
+                        padding: 20,
+                        margin: 5,
+                        borderRadius: 16,
+                    }}
+                >
+                    <Text style={{ fontSize: 16 }}>All</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: '#d3d3d3',
+                        padding: 20,
+                        margin: 5,
+                        borderRadius: 16,
+                    }}
+                >
+                    <Text style={{ fontSize: 16 }}>New</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: '#d3d3d3',
+                        padding: 20,
+                        margin: 5,
+                        borderRadius: 16,
+                    }}
+                >
+                    <Text style={{ fontSize: 16 }}>Reached</Text>
+                </TouchableOpacity>
+            </View>
+
+            <ActionButton
+                buttonColor="#63a79c"
+                onPress={() => navigation.navigate('Create')}
             />
         </View>
     );
