67c81cd3a06625026b2cca91a29cd74fef56c444
zegabr <jgsp2@cin.ufpe.br>
2020-04-22 21:16:48 -0300

    configurando linter

--- goals/.eslintrc.js
+++ goals/.eslintrc.js
@@ -1,25 +1,36 @@
 module.exports = {
-  env: {
-    es6: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+      es6: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
-};
+    extends: [
+      'airbnb',
+      'prettier',
+      'prettier/react', // integração do prettier com react
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parser: 'babel-eslint', // para entender as ultimas versões do EcmaScript
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+      'prettier', // adicionando mais um plugin
+    ],
+    rules: {
+      'prettier/prettier': 'error',
+      'react/jsx-filename-extension': [
+        'warn',
+        {
+          extensions: ['.jsx', '.js'],
+        },
+      ],
+      'import/prefer-default-export': 'off', // para garantir que import/export sem ser apenas o i/e default
+    },
+  };
